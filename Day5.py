8867bd5f8b53ee342a05c67578289b29d2678b8d
Steven Samuel <38133576+steven-thegamer@users.noreply.github.com>
2024-12-05 17:14:55 +0700

    Create Day5.py
    Solved Task 1, incomplete for Task 2

---
+++ Day5.py
@@ -0,0 +1,70 @@
+rules_front = {}
+rules_back = {}
+with open('Untitled2.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        key_value = line.split('|')
+        front_value = key_value[0]
+        back_value = key_value[1][0:-1]
+        if front_value in rules_front:
+            rules_front[front_value].append(back_value)
+        else:
+            rules_front[front_value] = [back_value]
+        if back_value in rules_back:
+            rules_back[back_value].append(front_value)
+        else:
+            rules_back[back_value] = [front_value]
+
+total = 0
+all_incorrect_rows = []
+
+# Task 1
+
+with open('Untitled3.txt') as g:
+    lines = g.readlines()
+    for line in lines:
+        whole_line = line[0:-1]
+        arr_line = whole_line.split(',')
+        n = len(arr_line)
+        good_row = True
+        for i in range(n):
+            for j in range(n):
+                if i < j:
+                    if arr_line[i] in rules_front and arr_line[j] not in rules_front[arr_line[i]]:
+                        good_row = False
+                        all_incorrect_rows.append(arr_line)
+                        break
+                elif i > j:
+                    if arr_line[i] in rules_back and arr_line[j] not in rules_back[arr_line[i]]:
+                        good_row = False
+                        all_incorrect_rows.append(arr_line)
+                        break
+            if good_row == False:
+                break
+        if good_row:
+            total += int(arr_line[n//2])
+print(total)
+
+# Task 2 (incomplete)
+
+for line in all_incorrect_rows:
+    n = len(line)
+    i = 0
+    j = 0
+    while i < n:
+        while j < n:
+            if i < j:
+                if line[j] in rules_back and line[i] in rules_back[line[i]]:
+                    line[i],line[j] = line[j],line[i]
+                    j = -1
+                    i = 0
+            elif i > j:
+                if line[j] in rules_front and line[i] in rules_front[line[i]]:
+                    line[i],line[j] = line[j],line[i]
+                    j = -1
+                    i = 0
+            j += 1
+        i += 1
+    total += int(line[n//2])
+
+print(total)
